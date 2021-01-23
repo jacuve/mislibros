cabced3c4213e7672d7f8af0c53b65fd3dfd105c
Jose Cuenca <jcuenca@monsol.net>
2021-01-23 23:33:54 +0100

    Step 2 . added tests

---
+++ tests/InitialPageTest.php
@@ -0,0 +1,14 @@
+<?php
+// tests/InitialPageTest.php
+namespace Tests\MisLibros;
+
+use PHPUnit\Framework\TestCase;
+
+class InitialPageTest extends TestCase
+{
+    public function testInitialList()
+    {
+        $this->assertTrue(true);
+    }
+
+}
---
+++ tests/LibroTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\MisLibros;
+
+use PHPUnit\Framework\TestCase;
+
+class LibroTest extends TestCase
+{
+    public function testInitialList()
+    {
+        $this->assertTrue(true);
+    }
+
+}
